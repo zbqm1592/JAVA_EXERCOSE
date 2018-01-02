27823fd6720235fb72d95c4515f5995bd71582dd
admin <admin@10.137.138.158>
2018-01-02 17:07:41 +0900

    chapter02

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 //		메인 메서드 안에 주석 넣기
 		System.out.println("Hello World~~");
 		System.out.println("준호따먹고싶다");
-		System.out.println("Sysout이라 입력하고 컨트롤 스페이스를 누르세요");
+		System.out.println("Sysout이라 입력하고  컨트롤 스페이스를 누르세요");
 		System.out.println("더블 퀀테이션 마크를 넣고 글을 입력한 뒤에   F11을 눌러보세여");
 	}
 
